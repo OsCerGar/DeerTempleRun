a15a45dfed06d3491e4048a68f9e3f91a9c660ac
OsCerGar <oscergar@gmail.com>
2018-03-20 13:26:11 +0100

    LOOP DONE MAH MEN

---
+++ DeerRun/Assets/RoadManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadManager : MonoBehaviour {
+    [SerializeField]
+    protected List<GameObject> roadPrefList = new List<GameObject>();
+    [SerializeField]
+    protected List<GameObject> bridgePrefList = new List<GameObject>();
+    [SerializeField]
+    protected GameObject postBridgePref;
+    [SerializeField]
+    protected  bool PostBridge = false;
+    [SerializeField]
+    float distTile;
+    float roadPos = 0;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 8; i++)
+        {
+            AddTile();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddTile()
+    {
+        if (PostBridge)//Para evitar caras invisibles :)
+        {
+            Debug.Log("post");
+            Instantiate(postBridgePref, new Vector3(0, 1, roadPos), Quaternion.identity);
+             PostBridge = false;
+        }
+        else
+        {
+            
+           var tile = Instantiate(roadPrefList[Random.Range(0, roadPrefList.Count)], new Vector3(0, 1, roadPos), Quaternion.identity);
+            if (tile.GetComponent<RoadPrefColl>().bridge)
+            {
+                PostBridge = true;
+            }
+        }
+        roadPos += distTile;
+        
+    }
+}
---
+++ DeerRun/Assets/RoadPrefColl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadPrefColl : MonoBehaviour {
+    private bool Destroy = false;
+    RoadManager RM;
+    public bool bridge;
+
+    private void Start()
+    {
+        RM = FindObjectOfType<RoadManager>();
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Destroy = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (Destroy)
+        {
+            StartCoroutine(DestroyAndCreate());
+        }
+    }
+
+    IEnumerator DestroyAndCreate()
+    {
+        yield return new WaitForSeconds(3);
+        RM.AddTile();
+        print("Nuevo Tile Creado");
+        Destroy(transform.gameObject);
+    }
+}
